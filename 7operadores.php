4317ba4c3e4ee0c6f34538c3307f09d996fe7692
David Calderón <dcalnav@gmail.com>
2026-09-25 17:00:57 +0200

    Ejercicio 7 de la relacion 1

---
+++ 7operadores.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores</title>
+</head>
+<body>
+<?php 
+
+    $nota1 = 8;
+    $nota2 = 3;
+    $faltas = 2;
+
+    $medianotas = ($nota1 + $nota2) / 2;
+    $notafinal = $medianotas - ($faltas*0.25);
+
+    if($notafinal >= 5){
+
+        echo "El alumno ha aprobado";
+
+    } else {
+
+        echo "El alumno ha suspendido";
+
+    }
+
+?>
+    
+</body>
+</html>
--- relacion1/5arraydiasemanatemp.php
+++ relacion1/5arraydiasemanatemp.php
@@ -1,50 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-const DIAS_SEMANA_TEMP = [
-    "Lunes" => "33",
-    "Martes" => "29",
-    "Miercoles" => "45",
-    "Jueves" => "35",
-    "Viernes" => "36",
-    "Sabado" => "28",
-    "Domingo" => "31"
-];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArrayConstanteAsociativo</title>
+</head>
 
-//Aqui muestro la temperatura que hacia el lunes:
-echo "El lunes hacia ".DIAS_SEMANA_TEMP["Lunes"]. " grados<br>";
+<body>
 
-//Aqui hago un foreach para recorrer el array e ir mostrando cada dia con su temperatura:
-foreach (DIAS_SEMANA_TEMP as $dia => $temperatura) {
-    echo "El ". $dia ." hacía ". $temperatura ." grados<br>";
-}
+    <?php
 
-echo "<ol>";
+    const DIAS_SEMANA_TEMP = [
+        "Lunes" => 33,
+        "Martes" => 29,
+        "Miercoles" => 45,
+        "Jueves" => 35,
+        "Viernes" => 36,
+        "Sabado" => 28,
+        "Domingo" => 31
+    ];
 
-//Hago lo mismo que el apartado anterior pero mostrandolo con una lista enumerada
-foreach (DIAS_SEMANA_TEMP as $dia => $temperatura) {
-    echo "<li>El ". $dia ." hacía ". $temperatura ." grados</li><br>";
-}
+    //Aqui muestro la temperatura que hacia el lunes:
+    echo "El lunes hacia " . DIAS_SEMANA_TEMP["Lunes"] . " grados<br>";
 
-echo "<table border='2'>";
+    //Aqui hago un foreach para recorrer el array e ir mostrando cada dia con su temperatura:
+    foreach (DIAS_SEMANA_TEMP as $dia => $temperatura) {
+        echo "El " . $dia . " hacía " . $temperatura . " grados<br>";
+    }
 
-// Le pongo cabecera a la tabla
-echo "<tr>";
-echo "<th>Día</th>";
-echo "<th>Temperatura</th>";
-echo "</tr>";
+    echo "<ol>";
 
-// Recorro el array y los muestrom uno a uno en formato de tabla
-foreach (DIAS_SEMANA_TEMP as $dia => $temperatura) {
-   
+    //Hago lo mismo que el apartado anterior pero mostrandolo con una lista enumerada
+    foreach (DIAS_SEMANA_TEMP as $dia => $temperatura) {
+        echo "<li>El " . $dia . " hacía " . $temperatura . " grados</li><br>";
+    }
+
+    echo "<table border='2'>";
+
+    // Le pongo cabecera a la tabla
     echo "<tr>";
-    echo "<td>" . $dia . "</td>";
-    echo "<td>" . $temperatura . " grados</td>";
+    echo "<th>Día</th>";
+    echo "<th>Temperatura</th>";
     echo "</tr>";
-}
 
-echo "</table>";
+    // Recorro el array y los muestrom uno a uno en formato de tabla
+    foreach (DIAS_SEMANA_TEMP as $dia => $temperatura) {
+
+        echo "<tr>";
+        echo "<td>" . $dia . "</td>";
+        echo "<td>" . $temperatura . " grados</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
 
 
 
 
-?>
+    ?>
+
+</body>
+
+</html>
