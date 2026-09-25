a1823840b473e549aea224a290f0cec884ea729d
David Calderón <dcalnav@gmail.com>
2026-09-25 16:22:18 +0200

    Ejercicio4

--- relacion1/4arraysemana.php
+++ relacion1/4arraysemana.php
@@ -11,21 +11,29 @@
 
     <?php
 
+    //Declaro el array constante con valores
     const DIAS_SEMANA = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 
+    //Muestro el primer dia de la semana con printf
+    echo "<h2>Primer dia de la semana:</h2>";
+    printf("Hoy es %s ", DIAS_SEMANA[0]);
 
-    echo DIAS_SEMANA[0] . "<br>";
+    //Muestro todos los dias de la semana con un for
+    echo "<h2>Dias de la semana:</h2>";
+    for ($i = 0; $i <= 6; $i++) {
 
-    foreach (DIAS_SEMANA as $DIA_SEMANA) {
+        echo "Día ". $i ." : ". DIAS_SEMANA[$i]. "<br>";
 
-        echo $DIA_SEMANA . "<br>";
     }
 
+    //Muestro los dias de la semana en forma de lista numerada
+    echo "<h2>Dias de la semana en lista ordenada:</h2>";
     echo "<ol>";
 
-    foreach (DIAS_SEMANA as $DIA_SEMANA) {
+    for ($i = 0; $i <= 6; $i++) {
 
-        echo "<li>" . $DIA_SEMANA . "</li><br>";
+        echo "<li>". DIAS_SEMANA[$i]. "<br></li>";
+
     }
 
     echo "</ol>";
