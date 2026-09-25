293c93d20b73f209c1eabfadb3a4d0d0adaf5268
David Calderón <dcalnav@gmail.com>
2026-09-25 15:58:20 +0200

    Ejercicio4

--- relacion1/4arraysemana.php
+++ relacion1/4arraysemana.php
@@ -1,24 +1,37 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArrayConstante</title>
+</head>
+
+<body>
+
+    <?php
 
     const DIAS_SEMANA = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 
 
-    echo DIAS_SEMANA[0]. "<br>";
+    echo DIAS_SEMANA[0] . "<br>";
 
-    foreach (DIAS_SEMANA as $DIA_SEMANA){
+    foreach (DIAS_SEMANA as $DIA_SEMANA) {
 
         echo $DIA_SEMANA . "<br>";
-
     }
 
     echo "<ol>";
 
-    foreach (DIAS_SEMANA as $DIA_SEMANA){
+    foreach (DIAS_SEMANA as $DIA_SEMANA) {
 
-        echo "<li>". $DIA_SEMANA . "</li><br>";
-
+        echo "<li>" . $DIA_SEMANA . "</li><br>";
     }
 
     echo "</ol>";
 
-?>
+    ?>
+
+</body>
+
+</html>
